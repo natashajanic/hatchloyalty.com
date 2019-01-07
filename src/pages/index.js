182a8247e5328b9f13ed843dcff4be6764312f9f
Dan Lavin <daniel.e.lavin@gmail.com>
2019-01-07 13:36:11 -0600

    design update

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,8 @@ import SEO from '../components/seo'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={['hatch', 'hatch loyalty', 'loyalty', 'personalization', 'activation']} />
-    <h1>Goodbye, traditional loyalty. Hello, Hatch. Technology designed to help you build stronger, more personal relationships with your customers.</h1>
+    <h1>Goodbye, traditional loyalty. Hello, Hatch.</h1>
+    <h2>Technology designed to help you build stronger, more personal relationships with your customers.</h2>
   </Layout>
 )
 
