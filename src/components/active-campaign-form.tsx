8890eea393f2c8c9fd39b49f2f7a946cb73c55c9
Dan Lavin <dlavin@hatchloyalty.com>
2019-02-25 15:20:17 -0600

    adds tests

--- src/components/active-campaign-form.tsx
+++ src/components/active-campaign-form.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import Helmet from 'react-helmet'
 
-interface IActiveCampaignFormProps {
+export interface IActiveCampaignFormProps {
   formID: string
   formSrc: string
 }
